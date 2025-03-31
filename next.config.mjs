535b2ff2acc68f7aa29087bc5c621b53a3e6d1a0
Ayush Anand <anandayush865@gmail.com>
2025-04-01 01:09:18 +0530

    Switch from static export to standard Next.js deployment for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
   },
   eslint: {
     // !! WARN !!
