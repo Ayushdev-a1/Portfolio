034cf1d42d74b6acb478880d4bc9fca581a1017e
Ayush Anand <anandayush865@gmail.com>
2025-04-01 01:00:01 +0530

    Add Vercel configuration to fix build issues

--- src/app/favicon.ts
+++ src/app/favicon.ts
@@ -1,12 +1,4 @@
-// This isn't working correctly, we'll use a simpler approach
-
-import { MetadataRoute } from 'next';
-
-// Use your profile image as favicon
-export default function Favicon(): MetadataRoute.Favicon {
-  return {
-    icon: '/Asset/ayush.jpg',
-    shortcut: '/Asset/ayush.jpg',
-    apple: '/Asset/ayush.jpg',
-  };
+// This is just a placeholder file, we're using the static favicon.ico instead
+export default function Favicon() {
+  return null;
 } 
