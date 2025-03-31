15b89ef5165964226961044ad0124052a501dff1
Ayush Anand <anandayush865@gmail.com>
2025-04-01 01:03:07 +0530

    Update Next.js config to explicitly disable ESLint and TypeScript checking during build

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,10 @@ const nextConfig = {
     unoptimized: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
+    // !! WARN !!
     ignoreDuringBuilds: true,
   },
   typescript: {
